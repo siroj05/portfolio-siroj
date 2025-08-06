90014c9d512a17cd3034b931f501330d4a81a0e7
sirojjuddin.apendi <sirojjuddin.apendi@amsconsult.com>
2025-08-06 11:18:02 +0700

    refactor

---
+++ components/section/about-section.tsx
@@ -0,0 +1,22 @@
+export default function AboutSection() {
+    return (
+        <section id="about">
+            <div className="space-y-10">
+                <h1 className="text-center font-bold text-4xl max-[426px]:text-3xl">About Me</h1>
+                <div className="max-w-4xl  max-[1024px]:max-w-2xl max-[691px] mx-auto">
+                    <p className="text-justify max-[426px]:text-[13px]">
+                        Passionate Frontend Developer with over 2 years of experience in
+                        developing scalable, high-performance web applications. Skilled
+                        in React.js, TypeScript, and Tailwind CSS, with a strong
+                        emphasis on clean, maintainable code and efficient user
+                        interactions. Enthusiastic about learning new technologies,
+                        tackling complex challenges, and staying up to date with
+                        industry advancements. Highly adaptable and always eager to
+                        explore new programming languages and frameworks to build
+                        innovative digital solutions.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
