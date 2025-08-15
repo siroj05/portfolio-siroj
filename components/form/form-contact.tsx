439f1e636f2f92ddce907a1cacbe64a63ac1f16b
sirojjuddin.apendi <sirojjuddin.apendi@amsconsult.com>
2025-08-15 16:46:09 +0700

    validasi form contact

--- components/form/form-contact.tsx
+++ components/form/form-contact.tsx
@@ -13,8 +13,8 @@ import { toast } from "sonner"
 import { useCreateMessage } from "@/api/messages"
 
 const formSchema = z.object({
-    email: z.email().min(1, "Email is required"),
-    message: z.string().min(1, "Message is required")
+    email: z.email().min(1, "Email is required").max(50, "Email too long"),
+    message: z.string().min(1, "Message is required").max(500, "Message too long")
 })
 
 type FormData = z.infer<typeof formSchema>
@@ -25,7 +25,8 @@ export default function FormContact() {
         handleSubmit,
         register,
         reset,
-        formState: { errors }
+        formState: { errors },
+        watch
     } = useForm<FormData>({
         resolver: zodResolver(formSchema)
     })
@@ -44,19 +45,28 @@ export default function FormContact() {
         mutate(data)
     }
 
+    const emailLength = watch("email")
+    const messageLength = watch("message")
+
     return (
         <div className="space-y-10 w-full">
             <div className="border dark:bg-zinc-800 p-4 rounded-sm">
                 <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
                     <div className="space-y-2">
                         <Label>Email</Label>
-                        <Input {...register("email")} type="email" placeholder="Email.." />
-                        <LabelError>{errors.email?.message}</LabelError>
+                        <Input {...register("email")} type="email" placeholder="Email.." maxLength={50}/>
+                        <div className="flex justify-between">
+                            <LabelError>{errors.email?.message}</LabelError>
+                            <p className="text-xs text-zinc-400">{emailLength?.length??0}/50</p>
+                        </div>
                     </div>
                     <div className="space-y-2">
                         <Label>Message</Label>
-                        <Textarea {...register("message")} placeholder="Message.." />
-                        <LabelError>{errors.message?.message}</LabelError>
+                        <Textarea {...register("message")} placeholder="Message.." maxLength={500}/>
+                        <div className="flex justify-between">
+                            <LabelError>{errors.message?.message}</LabelError>
+                            <p className="text-xs text-zinc-400">{messageLength?.length??0}/500</p>
+                        </div>
                     </div>
                     <Button className="w-full cursor-pointer" disabled={isPending}>
                         {
