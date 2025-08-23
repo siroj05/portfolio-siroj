e42bcb36c85b5cfc7ca312c033f1fae583532418
siroj05 <rojudin123@gmail.com>
2025-08-24 00:12:57 +0700

    fix date-picker

--- app/(admin)/experiences/add/page.tsx
+++ app/(admin)/experiences/add/page.tsx
@@ -7,7 +7,6 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import MinimalTiptapEditor from "@/components/ui/minimal-tiptap/minimal-tiptap";
-import { formatDate } from "@/lib/format-date";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod"
