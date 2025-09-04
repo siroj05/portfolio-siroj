8c56bfee05ac65e4b6122073c532785b97cf22a6
siroj05 <rojudin123@gmail.com>
2025-09-04 23:03:41 +0700

    fix end-date messages validation

--- app/(admin)/experiences/components/validation.ts
+++ app/(admin)/experiences/components/validation.ts
@@ -8,21 +8,21 @@ export const formSchema = z
     start: z.string().min(1, "Start From is required"),
     end: z.string().optional(),
     present: z.boolean(),
-    description: z.string().optional(),
+    description: z.string().min(1, "Description is required"),
   })
   .superRefine((data, ctx) => {
     if (!data.present && !data.end) {
       ctx.addIssue({
-        path: ["to"],
+        path: ["end"],
         code: z.ZodIssueCode.custom,
         message: "To is required",
       });
     }
-
+    
     // validasi range date
     if (data.end && data.start > data.end) {
       ctx.addIssue({
-        path: ["to"],
+        path: ["end"],
         code: z.ZodIssueCode.custom,
         message: "To date cannot be earlier than start from",
       });
